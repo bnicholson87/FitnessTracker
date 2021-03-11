38b1f8b0a80d86fc9bde9a8c35ce003d9899e14d
bnicholson87 <bnicholson091@gmail.com>
2021-03-11 15:32:32 -0500

    Updated workout.js

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+      type: Date,
+      default: Date.now
+  },
+  exercises: [
+      {
+          type: {
+              type: String,
+              required: true
+          },
+          name: {
+              type: String,
+              required: true
+          },
+          duration: {
+              type: Number,
+              required: true
+          },
+          distance: {
+              type: Number,
+              required: false
+          },
+          weight: {
+              type: Number,
+              required: false
+          },
+         reps: {
+             type: Number,
+             required: false
+         },
+         sets: {
+             type: Number,
+             required: false
+         } 
+      }
+  ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
